56d9690695cb68796b6a4096879678165d1e419f
alexandr.tutunaru <atutunaru@deloittece.com>
2025-02-14 15:31:19 +0200

    Commit New Updates

---
+++ secondProject/src/JavaLogic1/cigarParty.java
@@ -0,0 +1,11 @@
+public boolean cigarParty(int cigars, boolean isWeekend) {
+    if (isWeekend && cigars >=40)
+        return true;
+
+    if (!(isWeekend) && cigars >=40 && cigars <=60)
+        return true;
+
+    else
+        return false;
+
+}
